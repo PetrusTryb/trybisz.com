adcf03d8c345b5473bbc54b2fe5c2223d9d6368a
Piotr Trybisz <ptrybisz@gmail.com>
2023-07-11 00:14:41 +0200

    Not using middleware to fetch project data anymore

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client-image" />
-declare namespace App {
-    interface Locals {
-        getProject: (slug:string) => Promise<any>;
-        getAllProjects: () => Promise<any>;
-    }
-}
